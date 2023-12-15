9efc26ab0d804c270af5195c956698a23062a681
Séverine <severine.zapata@orange.fr>
2023-12-15 10:40:10 +0100

    correction route page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   //http://localhost:4200
@@ -11,6 +12,8 @@ const routes: Routes = [
   { path: 'my-plants', component: PageMyPlantsComponent },
   // Set route for url : http:/localhost:4200/admin
   { path: 'admin', component: PageAdminComponent },
+  // Page 404
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({

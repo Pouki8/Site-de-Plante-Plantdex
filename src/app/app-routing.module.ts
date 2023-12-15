5845901cccc35e0a9ec260211337e29957cb6784
Séverine <severine.zapata@orange.fr>
2023-12-15 10:10:28 +0100

    routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //http://localhost:4200
+  { path: '', component: PageHomeComponent },
+  //Set route for url : http:/localhost:4200/my-plants
+  { path: 'my-plants', component: PageMyPlantsComponent },
+  // Set route for url : http:/localhost:4200/admin
+  { path: 'admin', component: PageAdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

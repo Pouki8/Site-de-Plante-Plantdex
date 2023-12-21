4b25230302231f16e6de23a47ca09e8cc2499611
Séverine <severine.zapata@orange.fr>
2023-12-21 16:15:56 +0100

    plant card et communication parent enfant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CardPlantComponent } from './componenents/card-plant/card-plant.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     PageHomeComponent,
     PageMyPlantsComponent,
     PageAdminComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    CardPlantComponent
   ],
   imports: [
     BrowserModule,
